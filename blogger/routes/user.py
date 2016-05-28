bb8b472e437229cba99a059bbaea8bc1f2204091
Abe Kim <abekim0607@gmail.com>
2016-05-28 15:00:15 -0700

    Adding users. Not working atm

---
+++ blogger/routes/user.py
@@ -0,0 +1,37 @@
+"""
+author: @abekim
+"""
+
+from blogger import app
+from flask import render_template
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # next_is_valid should check if the user has valid
+        # permission to access the `next` url
+        if not next_is_valid(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+
+    if request.method = 'POST':
+        login()
+    else:
+        show_login_form()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
